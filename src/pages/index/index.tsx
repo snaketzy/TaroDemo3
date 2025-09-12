dc332a3eb25a2a950cc29eb71f918b380c212210
tangzy <tangzy@zhenyetong.com>
2025-09-12 14:12:30 +0800

    feat:增加prettier

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
-import { Component, PropsWithChildren, useEffect } from 'react'
-import { connect, useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { View, Button, Text } from '@tarojs/components'
 // import { ApplicationState } from "../../store";
 import { updateCommonState } from "../../store/commonSlice";
@@ -21,26 +21,26 @@ interface Props {
   children?: React.ReactNode;
 }
 
-const Index = (props:Props) => {
+const Index = (props: Props) => {
 
   const commonModule = useSelector((state: ApplicationState) => state.commonSlice);
   const dispatch = useDispatch();
 
   useEffect(() => {
 
-  },[props])
+  }, [props])
 
   useEffect(() => {
     return () => {
       console.log("Component will unmount")
     }
-  },[])
+  }, [])
 
-  
+
   return (
     <View className='index'>
-      <Button className='add_btn' onClick={ () => dispatch(updateCommonState({count:{num: commonModule.count.num + 1}})) }>+</Button>
-      <Button className='dec_btn' onClick={ () => dispatch(updateCommonState({count:{num: commonModule.count.num - 1}})) }>-</Button>
+      <Button className='add_btn' onClick={() => dispatch(updateCommonState({ count: { num: commonModule.count.num + 1 } }))}>+</Button>
+      <Button className='dec_btn' onClick={() => dispatch(updateCommonState({ count: { num: commonModule.count.num - 1 } }))}>-</Button>
       <View><Text>{commonModule.count.num}</Text></View>
       <View><Text>测试数据</Text></View>
     </View>
